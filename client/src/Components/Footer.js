1e917492fdb9541fd7b789c3a2dec694618d4dd0
Alexander <alexander.botello@g.austincc.edu>
2018-09-01 19:42:02 -0500

    Add made with bulma badge

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Footer, Container, Icon } from 'bloomer';
+import { Section, Footer, Container, Icon } from 'bloomer';
 
 const Foot = props => {
   return(
+    <Section>
+
     <Footer id="footer">
       <Container>
-        <p>Made with <Icon hasTextColor='danger' className="fa fa-heart"></Icon> by Alexander Botello</p>
+        <a href="https://bulma.io">
+          <img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="128" height="24"/>
+        </a>
+        <p>
+          Made with <Icon isSize="small" hasTextColor="danger" className="fas fa-heart"></Icon> by <a href="https://github.com/alexbotello">Alexander Botello</a>
+        </p>
       </Container>
     </Footer>
+    </Section>
   )
 }
 export default Foot;
