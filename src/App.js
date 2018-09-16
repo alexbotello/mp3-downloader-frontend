a18fa9170a90efa28d5f50ab83563d44ff680423
Alexander <alexander.botello@g.austincc.edu>
2018-09-16 17:58:10 -0500

    Add disclaimer message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Banner from './Components/Banner';
 import Form from './Components/Form';
+import Message from './Components/Message';
 import Foot from './Components/Footer';
 import './App.css';
 
@@ -12,6 +13,7 @@ class App extends Component {
       <div className="App">
         <Banner />
         <Form />
+        <Message />
         <Foot />
       </div>
     );
---
+++ src/Components/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Title } from 'bloomer';
+
+export default props => {
+  return (
+    <div className="disclaimer">
+      <Title isSize={6}>
+        Disclaimer: Audio conversion may take up to 30-40 seconds
+      </Title>
+    </div>
+  )
+}
